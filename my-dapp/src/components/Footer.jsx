0afbb440bb46ee144cb1815864ad3f1a92ad2f7f
Githaiga22 <allangithaiga5@gmail.com>
2025-05-09 22:32:12 +0300

    add Footer component for copyright information

---
+++ my-dapp/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ background: "#223a5a", color: "#b0c4d4", textAlign: "center", padding: "1.5rem 0", fontSize: 16, marginTop: "auto" }}>
+      Copyright © 2025 PixelShield. All Rights Reserved
+    </footer>
+  );
+} 
