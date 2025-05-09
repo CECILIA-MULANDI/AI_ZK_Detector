1fe12875c1bcff4ee46c36b557b02671d63d07d4
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-05-10 01:53:26 +0300

    cleaned up code

--- AI_Detection_Model/model.py
+++
@@ -1,6 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("image-classification", model="umm-maybe/AI-image-detector")
-result = pipe("path/to/image.jpg")
-print(result) 
